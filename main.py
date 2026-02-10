458a8d002c6826bcafc0d290dc146b519e2347b7
Lalith Amudala <lalithamudala7@gmail.com>
2026-02-10 16:41:48 -0600

    updated files

---
+++ main.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches
+    
+    :param query: The query to search for
+    :type query: str
+    :return: The Search result
+    :rtype: str
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
+
+llm = ChatOpenAI(model="gpt-5")
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tyokyo")})
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
